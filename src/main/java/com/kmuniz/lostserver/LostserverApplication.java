48c51a64d70cc295cf5f593d7ca4e3b733d15753
Karel <oricha@gmail.com>
2024-12-27 11:15:42 +0100

    Added project

---
+++ src/main/java/com/kmuniz/lostserver/LostserverApplication.java
@@ -0,0 +1,13 @@
+package com.kmuniz.lostserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmuniz/lostserver/LostserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmuniz.lostserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
